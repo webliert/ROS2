60ea37e8b8b94eacb04cd0837fbfca5700ba7bc1
Yan <1980602463@qq.com>
2024-10-14 17:14:05 +0800

    message: "hello_ROS2"

---
+++ hello_ROS2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello ROS2! test git"<<endl;
+    cout<<"the enviroment is linux of Ubuntu 22.04, and the compiler is g++ 11.2.0."<<endl;
+    return 0;
+}
